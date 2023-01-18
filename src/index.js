67c4a7673fc2c759c82bf7bcd53bba27c5f1ad05
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-01-18 15:26:15 -0500

    Remove the root creation from App.jsx and implement it here keeping in mind the new version of react and react-dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
 import './css/body.css';
 import './css/app.css';
 import './css/header.css';
 
 import App from './App.jsx';
 
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <App />
+);
