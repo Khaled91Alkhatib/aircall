c7cb400abfd17fb4e38005f6782408fbdffb189b
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-01-19 09:19:14 -0500

    Create code to handle data from state and organize results

---
+++ src/components/AllCalls.jsx
@@ -0,0 +1,45 @@
+import React, { useContext, useState } from 'react';
+import GeneralContext from '../contexts/GeneralContext';
+
+import { SlCallIn, SlCallOut } from 'react-icons/sl';
+import { FiArchive } from 'react-icons/fi';
+import "../css/AllCalls.css";
+
+const AllCalls = () => {
+  const { calls, setArchivedCalls, archivedCalls } = useContext(GeneralContext);
+  console.log("single calls", calls);
+
+  return (
+    <div>
+      {calls.map((call) => (
+        <div key={call.id}>
+          <div className='date'>{new Date(call.created_at).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })}</div>
+          <div className='main-allcalls'>
+            <div className='call-info'>
+              <div className='icon-details'>
+                <div>
+                  <div style={{ marginRight: "1em" }}>{call.direction === "inbound" ? <SlCallIn style={{ color: "green", fontSize: "17px" }} /> : <SlCallOut style={{ color: "red", fontSize: "17px" }} />}</div>
+                </div>
+                <div>
+                  <div style={{ fontWeight: "bold" }}>{call.from}</div>
+                  <div style={{ color: "grey" }}>{call.direction === "inbound" ? `Incoming call from ${call.from}` : `Outgoing call to ${call.to}`}</div>
+                </div>
+              </div>
+              <div className='time-archive'>
+                <div style={{ color: "grey" }}>{new Date(call.created_at).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</div>
+                <div className='border-dot-inarchive'>
+                  <div className='border-inarchive' />
+                  <div className='border-inarchive' />
+                  <div className='border-inarchive' />
+                </div>
+                <button onClick={(prev) => { setArchivedCalls([...archivedCalls, call]); }} className="archive-button"><FiArchive className='archive-icon' /></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AllCalls;
