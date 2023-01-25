af73704af29c3ab84f29160e0018110c3f95eb2f
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-01-25 16:57:45 -0500

    Implement code to handle moving calls into and out of archive

--- src/components/AllCalls.jsx
+++ src/components/AllCalls.jsx
@@ -4,46 +4,59 @@ import GeneralContext from '../contexts/GeneralContext';
 import { SlCallIn, SlCallOut } from 'react-icons/sl';
 import { FiArchive } from 'react-icons/fi';
 import "../css/AllCalls.css";
+import axios from 'axios';
 
 const AllCalls = () => {
   const { calls, setCalls, setArchivedCalls, archivedCalls } = useContext(GeneralContext);
   // console.log("single calls", calls);
 
-  const onArchive = (call) => {
-    setArchivedCalls([...archivedCalls, call]);
-  };
-  const removeFromCalls = (id) => {
-    setCalls((pre) => pre.filter((item) => !(id === item.id)));
+  const handleArchiveCall = (id) => {
+    axios.patch(`https://cerulean-marlin-wig.cyclic.app/activities/${id}`, { is_archived: true })
+      .then((res) => {
+        const updatedCalls = calls.map(call => {
+          if (call.id === id) {
+            call.is_archived = true;
+          }
+          return call;
+        });
+        setCalls(updatedCalls);
+        // console.log("updatecalls", updatedCalls);
+        // console.log('archive thing', res);
+      });
   };
 
   return (
     <div className='main-archived-calls'>
       {calls.map((call) => (
-        <div key={call.id}>
-          <div className='date'>{new Date(call.created_at).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })}</div>
-          <div className='main-allcalls'>
-            <div className='call-info'>
-              <div className='icon-details'>
-                <div>
-                  <div style={{ marginRight: "1em" }}>{call.direction === "inbound" ? <SlCallIn style={{ color: "green", fontSize: "17px" }} /> : <SlCallOut style={{ color: "red", fontSize: "17px" }} />}</div>
+        call.is_archived === false
+          ?
+          <div key={call.id}>
+            <div className='date'>{new Date(call.created_at).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })}</div>
+            <div className='main-allcalls'>
+              <div className='call-info'>
+                <div className='icon-details'>
+                  <div>
+                    <div style={{ marginRight: "1em" }}>{call.direction === "inbound" ? <SlCallIn style={{ color: "green", fontSize: "17px" }} /> : <SlCallOut style={{ color: "red", fontSize: "17px" }} />}</div>
+                  </div>
+                  <div>
+                    <div style={{ fontWeight: "bold" }}>{call.from}</div>
+                    <div style={{ color: "grey" }}>{call.direction === "inbound" ? `Incoming call from ${call.from}` : `Outgoing call to ${call.to}`}</div>
+                  </div>
                 </div>
-                <div>
-                  <div style={{ fontWeight: "bold" }}>{call.from}</div>
-                  <div style={{ color: "grey" }}>{call.direction === "inbound" ? `Incoming call from ${call.from}` : `Outgoing call to ${call.to}`}</div>
+                <div className='time-archive'>
+                  <div style={{ color: "grey" }}>{new Date(call.created_at).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</div>
+                  <div className='border-dot-inarchive'>
+                    <div className='border-inarchive' />
+                    <div className='border-inarchive' />
+                    <div className='border-inarchive' />
+                  </div>
+                  <button onClick={() => { handleArchiveCall(call.id); }} className="archive-button"><FiArchive className='archive-icon' /></button>
                 </div>
-              </div>
-              <div className='time-archive'>
-                <div style={{ color: "grey" }}>{new Date(call.created_at).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</div>
-                <div className='border-dot-inarchive'>
-                  <div className='border-inarchive' />
-                  <div className='border-inarchive' />
-                  <div className='border-inarchive' />
-                </div>
-                <button onClick={() => { onArchive(call); removeFromCalls(call.id); }} className="archive-button"><FiArchive className='archive-icon' /></button>
               </div>
             </div>
           </div>
-        </div>
+          :
+          ""
       ))}
     </div>
   );
--- src/components/ArchivedCalls.jsx
+++ src/components/ArchivedCalls.jsx
@@ -4,27 +4,35 @@ import GeneralContext from '../contexts/GeneralContext';
 import { SlCallIn, SlCallOut } from 'react-icons/sl';
 import { TbArchiveOff } from 'react-icons/tb';
 import "../css/AllCalls.css";
+import axios from 'axios';
 
 
 const ArchivedCalls = () => {
   const { calls, setCalls, archivedCalls, setArchivedCalls } = useContext(GeneralContext);
   // console.log('halla2', archivedCalls);
 
-  const onAddToCalls = (archivedCall) => {
-    setCalls([...calls, archivedCall]);
+  const removeArchiveCall = (id) => {
+    axios.patch(`https://cerulean-marlin-wig.cyclic.app/activities/${id}`, { is_archived: false })
+      .then((res) => {
+        const updatedCalls = calls.map(call => {
+          if (call.id === id) {
+            call.is_archived = false;
+          }
+          return call;
+        });
+        setCalls(updatedCalls);
+        // console.log("updatecalls", updatedCalls);
+        // console.log('archive thing', res);
+      });
   };
 
-  const onRemoveClick = (id) => {
-    setArchivedCalls((pre) => pre.filter((item) => !(id === item.id)));
-  };
 
   return (
     <div style={{ marginTop: "55px" }}>
-      {archivedCalls.length === 0 ?
-        <div>No Archived Calls</div>
-        :
-        <div className='main-archived-calls'>
-          {archivedCalls.map((archivedCall) => (
+      <div className='main-archived-calls'>
+        {calls.map((archivedCall) => (
+          archivedCall.is_archived === true
+            ?
             <div key={archivedCall.id}>
               <div className='date'>{new Date(archivedCall.created_at).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' })}</div>
               <div className='main-allcalls'>
@@ -45,16 +53,16 @@ const ArchivedCalls = () => {
                       <div className='border-inarchive' />
                       <div className='border-inarchive' />
                     </div>
-                    <button onClick={() => { onAddToCalls(archivedCall); onRemoveClick(archivedCall.id); }} className="archive-button"><TbArchiveOff className='archive-icon' /></button>
+                    <button onClick={() => { removeArchiveCall(archivedCall.id); }} className="archive-button"><TbArchiveOff className='archive-icon' /></button>
                   </div>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
-      }
+            :
+            ""
+        ))}
+      </div>
     </div>
-
   );
 };
 
